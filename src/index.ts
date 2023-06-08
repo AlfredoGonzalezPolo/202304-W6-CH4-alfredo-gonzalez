d702ff8f20d0904792275bf019ea400e8ae8d46c
Alfredo González Polo <fredigonzapolo@hotmail.com>
2023-06-09 00:17:21 +0200

    Add case errors and delete commander

--- src/index.ts
+++ src/index.ts
@@ -24,36 +24,33 @@ const multiplication = (a: number, b: number) => a * b;
 
 const server = http.createServer((req, res) => {
   if (!req.url) {
-    server.emit('error', new Error('No url in the request'));
+    server.emit('error', new Error('Error 404'));
+    res.write(`<h1>Error 404</h1>`);
     return;
   }
 
-  const pathname = url.parse(req.url);
-  const queryParams = pathname.query;
+  const urlName = url.parse(req.url);
+  const queryParams = urlName.query;
   const queryParamsValues = new URLSearchParams(queryParams!);
   const number1 = parseFloat(queryParamsValues.get('a')!);
   const number2 = parseFloat(queryParamsValues.get('b')!);
 
-  if (req.method !== 'GET') {
-    server.emit('error', new Error('Invalid method'));
+  if (urlName.toString() !== '/calculator') {
+    server.emit('error', new Error('Path not found'));
+    res.write(`<h1>Error 404</h1><h2>Path not found</h2>`);
     return;
   }
 
   res.write(`<p>${number1} + ${number2} = ${add(number1, number2)}</p>`);
   res.write(`<p>${number1} - ${number2} = ${rest(number1, number2)}</p>`);
-  res.write(`<p>${number1} * ${number2} = ${division(number1, number2)}</p>`);
+  res.write(`<p>${number1} / ${number2} = ${division(number1, number2)}</p>`);
   res.write(
-    `<p>${number1} / ${number2} = ${multiplication(number1, number2)}</p>`
+    `<p>${number1} * ${number2} = ${multiplication(number1, number2)}</p>`
   );
-  res.write(req.method);
   res.end();
 });
 
 server.listen(PORT);
-
-server.on('listening', () => {
-  console.log('Listening on port ' + PORT);
-});
 
 server.on('error', (error) => {
   console.log(error.message);
